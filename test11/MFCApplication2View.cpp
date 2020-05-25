c129089e71146b89ce32dab0b7c14ba008e37639
DJQ0926 <964158009@qq.com>
2020-05-25 12:52:23 +0800

    正式作业1

---
+++ test11/MFCApplication2View.cpp
@@ -0,0 +1,125 @@
+
+// MFCApplication2View.cpp : CMFCApplication2View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFCApplication2.h"
+#endif
+
+#include "MFCApplication2Doc.h"
+#include "MFCApplication2View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication2View
+
+IMPLEMENT_DYNCREATE(CMFCApplication2View, CView)
+
+BEGIN_MESSAGE_MAP(CMFCApplication2View, CView)
+	ON_WM_NCLBUTTONDOWN()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMFCApplication2View 构造/析构
+
+CMFCApplication2View::CMFCApplication2View()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFCApplication2View::~CMFCApplication2View()
+{
+}
+
+BOOL CMFCApplication2View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCApplication2View 绘制
+
+void CMFCApplication2View::OnDraw(CDC* pDC)
+{
+	CMFCApplication2Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	/*CString s = _T("我是大学生");
+	int a = 21;
+	CString A;
+	A.Format(_T("%d"), a);
+	pDC->TextOutW(200, 100, s);
+	pDC->TextOutW(200, 200, A);*/
+
+	//pDC->TextOutW(200, 100, pDoc->S);
+	//pDC->TextOutW(200, 200, pDoc->A); 
+
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMFCApplication2View 诊断
+
+#ifdef _DEBUG
+void CMFCApplication2View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCApplication2View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCApplication2Doc* CMFCApplication2View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCApplication2Doc)));
+	return (CMFCApplication2Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCApplication2View 消息处理程序
+
+
+
+
+void CMFCApplication2View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	CMFCApplication2Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	pDoc->count += 1;
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CView::OnLButtonDown(nFlags, point);
+}
+
+
+void CMFCApplication2View::OnRButtonUp(UINT nFlags, CPoint point)
+{
+	CMFCApplication2Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CView::OnRButtonUp(nFlags, point);
+	CClientDC dc(this);
+	CString xx;
+	xx.Format(_T("%d"), pDoc->count);
+	dc.TextOutW(600, 600, xx);
+}
